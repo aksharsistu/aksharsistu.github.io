b3a894cbaa966f56503e9802fb013b80ba52c0ed
Sistu Akshar <f20212598@goa.bits-pilani.ac.in>
2022-05-22 04:27:12 +0530

    added media queries; fixed footer; minor font changes

--- javaScript/image_loop.js
+++ javaScript/image_loop.js
@@ -13,6 +13,14 @@ function image_loop(element) {
         } else if (img === 2) {
             element.style.opacity = '0'
             element.style.visibility = 'collapse'
+            element.src = "images/banner/3.png"
+            img = 3
+            console.log(img)
+            element.style.visibility = 'visible'
+            element.style.opacity = '1'
+        }else if (img === 3) {
+            element.style.opacity = '0'
+            element.style.visibility = 'collapse'
             element.src = "images/banner/1.png"
             img = 1
             console.log(img)
